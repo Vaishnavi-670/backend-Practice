4fd0b58f29a0c0410812c209f2190a5b46ff799b
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-08-07 20:27:09 +0530

    ..

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express'); //this line is used to import the express module  which is a minimal and flexible Node.js web application framework.
+const UserRouter = require('./routers/userRouter');
+
 //initialize express
 const app = express(); //this line creates an instance of the express module.
 const port =5000; //this line is used to set the port number to 5000
 
-app.listen(port ,() =>{console.log('Server is running ')}); //this line is used to start the server on port 5000 and print a message on the console.
+//middleware
+app.use('/user', UserRouter); //this line is used to use the userRouter as a middleware in the express app. This means that all the routes defined in the userRouter will be accessible under the /user path.
+
+
+//route or express
+app.get('/', (req, res) => { //this line is used to create a route that listens to the root URL of the website.
+    res.send('Response from express'); //this line is used to send a response to the client when the route is accessed.
+});
+app.get('/add',(req, res) => {
+    res.send('Response from add route');
+});
+
+app.listen(port ,( ) =>{console.log('Server is running ')}); //this line is used to start the server on port 5000 and print a message on the console.
 
