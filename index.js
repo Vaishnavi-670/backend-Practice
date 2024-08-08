2c60c79e2724ee1c4087a426369916d5fa61304e
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-08-08 20:32:41 +0530

    ..

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const app = express(); //this line creates an instance of the express module.
 const port =5000; //this line is used to set the port number to 5000
 
 //middleware
-app.use('/user', UserRouter); //this line is used to use the userRouter as a middleware in the express app. This means that all the routes defined in the userRouter will be accessible under the /user path.
 
+app.use(express.json());
+app.use('/user', UserRouter); //this line is used to use the userRouter as a middleware in the express app. This means that all the routes defined in the userRouter will be accessible under the /user path.
 
 //route or express
 app.get('/', (req, res) => { //this line is used to create a route that listens to the root URL of the website.
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/add',(req, res)=>{
+router.post('/add',(req, res)=>{
+    console.log(req.body);
+    
     res.send('Response from express');
 });
 
@@ -10,7 +12,8 @@ router.get('/getall',(req, res)=>{
     res.send('Response from getall');
 });
 //getbyid
-router.get('/getbyid',(req, res)=>{
+router.get('/getbyid/:id',(req, res)=>{
+    console.log(req.params.id); // to get the id from the url parameters. params means the parameters passed in the url.
     res.send('Response from getbyid');
 });
 //update
