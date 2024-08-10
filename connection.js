6b88a56e8f78a252c67a0e09164bf0f2af66817a
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-08-11 02:53:34 +0530

    ..

--- connection.js
+++ connection.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://vaishnavi357:Vaishnavi123@cluster0.k4yrt8l.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0"
+const url = "mongodb+srv://vaishnavi357:vaishnavi123@cluster0.k4yrt8l.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0"
 
+//connect to database 
+
+//asynchronous function-return a promise
 mongoose.connect(url) 
+.then((result) => {
+    console.log('connected to database');
+}).catch((err) => {
+    console.log(err);
+});
